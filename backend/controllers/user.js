f041f07e108c9eb8f039337ba92e1cc2239a1f9f
yao-le <le.yao@northeastern.edu>
2023-01-02 19:23:06 -0500

    add emailToken model

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -10,5 +10,14 @@ exports.create = async (req, res) => {
   const newUser = new User({ name, email, password });
   await newUser.save();
 
+  // var transport = nodemailer.createTransport({
+  //   host: "smtp.mailtrap.io",
+  //   port: 2525,
+  //   auth: {
+  //     user: "f29863e00f4147",
+  //     pass: "0df2fe5327c2cf"
+  //   }
+  // });
+
   res.status(201).json({ user: newUser });
 };
